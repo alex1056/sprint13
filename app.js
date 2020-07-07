6b6ae4b840f91eef2ede85a47cd521dd78cf242c
alexander.b <gituser1056@gmail.com>
2020-07-07 13:42:14 +0300

    Убрали папку data, public, helpers

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   req.user = {
